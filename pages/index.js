8c72dd06454a402b708349674440282285ce3397
wangshixiao <wangshixiao@neusoft.com>
2023-04-11 02:05:33 +0800

    chore: add layout

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,9 @@ import Box from '@mui/material/Box';
 import ProTip from '../src/ProTip';
 import Link from '../src/Link';
 import Copyright from '../src/Copyright';
+import Layout from "../components/layout";
 
-export default function Index() {
+export default function Page() {
   return (
     <Container maxWidth="sm">
       <Box sx={{ my: 4 }}>
@@ -14,18 +15,25 @@ export default function Index() {
           Next.js example
         </Typography>
         <Link href="/about" color="secondary">
-          Go to the about page
+          Go to the about page1
         </Link>
 
         <Link href="/login" color="secondary">
-          Go to the login page
+          Go to the login page2
         </Link>
         <Link href="/home" color="secondary">
-          Go to the home page
+          Go to the home page3
         </Link>
         <ProTip />
         <Copyright />
       </Box>
     </Container>
   );
+}
+Page.getLayout = function getLayout(page) {
+  return (
+      <Layout>
+        {page}
+      </Layout>
+  )
 }

f00ed6608b1027b23dd67bc8f61f8a5dd32ac1ba
wsxok <wsxok15@163.com>
2023-05-20 23:04:15 +0800

    chore: layout调整

--- components/layout.js
+++ components/layout.js
@@ -1,12 +1,21 @@
 import Navbar from './navbar'
 import Footer from './footer'
+import Box from "@mui/material/Box";
 
 export default function Layout({ children }) {
     return (
         <>
-            <Navbar />
-            <main>{children}</main>
-            <Footer />
+            <Box sx={{ display: 'flex',
+                flexDirection: 'row', }}>
+                <Box>
+                    <Navbar />
+                </Box>
+                <Box sx={{ flexGrow: 1 }}>
+                    <main >{children}</main>
+                    <Footer />
+                </Box>
+            </Box>
+
         </>
     )
 }
